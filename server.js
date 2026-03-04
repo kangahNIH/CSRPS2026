79af4e7c86161f6f67006945189f7c426d015636
kangahNIH <kangah@nih.gov>
2026-03-04 16:39:18 -0500

    Set up basic HTTP server with success message

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Success! Your Azure Web App is running.</h1><p>You can now start building your user interface here.</p>');
+});
+
+// Azure App Service provides the port to listen on via an environment variable
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
 
